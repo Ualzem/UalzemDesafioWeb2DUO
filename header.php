7adbf82dd9f0e68edd5fab24a5ce94c6fe65050f
Ualzem <arquivosualzem@gmail.com>
2021-05-28 14:23:41 -0300

    Melhorias Responsivo

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <meta property="og:url" content="<?php bloginfo('url'); ?>"/>
         <meta property="og:image" content=" <?php echo get_template_directory_uri(); ?> /img/logo.png"/>
 
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='viewport' content='width=device-width, user-scalable=0'>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 
     <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
